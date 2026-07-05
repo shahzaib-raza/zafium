e93aa94c8497f11554253b664d06e26e456aa77b
Shahzaib Raza <64272560+shahzaib-raza@users.noreply.github.com>
2026-07-05 13:53:53 +0500

    update email credits

--- zafium/settings.py
+++ zafium/settings.py
@@ -131,7 +131,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "zafium.technologies.pk@gmail.com"
-EMAIL_HOST_PASSWORD = "rzijmfxivaagfrxl"
+EMAIL_HOST_USER = "zafium.tech@gmail.com"
+EMAIL_HOST_PASSWORD = "sremsaebodpkqqtz"
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
