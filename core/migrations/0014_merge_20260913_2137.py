1a3f0dedc07e85107a8baae79a8b632e04f560d4
Shahzaib Raza <64272560+shahzaib-raza@users.noreply.github.com>
2026-09-14 09:57:18 +0500

    Update 0014_merge_20260913_2137.py

--- core/migrations/0014_merge_20260913_2137.py
+++ core/migrations/0014_merge_20260913_2137.py
@@ -6,7 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0011_orderattachment"),
         ("core", "0013_order_currency_orderattachment"),
     ]
 
